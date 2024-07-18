584dd51e07e69ae0dc294620fe0c10abf50c83e2
sanan <sanan3946@gmail.com>
2024-07-18 13:50:35 +0530

    Core_Service init and Prepared docker-compose.yml to Configure Backend Servers

---
+++ Core_Service/src/test/java/com/Core_Service/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Core_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
