ae3566b54eeda2ef070adce603b0979f8879c91d
sanan <sanan3946@gmail.com>
2024-07-24 19:08:33 +0530

    Stream_Service project init

---
+++ Core_Service/src/main/java/com/Core_Service/config_kafka/Check.java
@@ -0,0 +1,10 @@
+package com.Core_Service.config_kafka;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Check {
+
+
+
+}
---
+++ Stream_Service/src/test/java/com/Stream_Service/StreamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stream_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
